73dd81d50d18d73e0640a6c90561f9ab142963ea
akannke <psse111@gmail.com>
2020-01-01 10:56:57 +0900

    Save to file

--- collector.py
+++ collector.py
@@ -1,7 +1,11 @@
 import socketio
+import os
+import csv
 
 ENDPOINT_URL = "https://io.lightstream.bitflyer.com"
 PUBLIC_CHANNEL = "lightning_executions_FX_BTC_JPY"
+
+FILE_NAME = "executions.csv"
 
 sio = socketio.Client()
 
@@ -19,12 +23,24 @@ def connect():
 
 @sio.event
 def disconnect():
-    print('disconnected from server')
+    print("disconnected from server")
 
 @sio.on(PUBLIC_CHANNEL)
-def receive_event(msg):
-    print(msg)
+def receive_event(msgs):
+    with open(FILE_NAME, "a") as f:
+        fieldnames = ["id", "side", "price", "size", "exec_date", "buy_child_order_acceptance_id", "sell_child_order_acceptance_id"] 
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        for msg in msgs:
+            writer.writerow(msg)
 
-sio.connect(ENDPOINT_URL, transports="websocket")
-sio.wait()
+
+if __name__ == "__main__":
+    if os.path.isfile(FILE_NAME):
+        with open(FILE_NAME, "w") as f:
+            fieldnames = ["id", "side", "price", "size", "exec_date", "buy_child_order_acceptance_id", "sell_child_order_acceptance_id"] 
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+
+    sio.connect(ENDPOINT_URL, transports="websocket")
+    sio.wait()
 
